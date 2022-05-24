66a939846893bd900bdf7c8e685600c3dd928f4a
Marco <jonghyeok.lim14@gmail.com>
2022-05-24 18:25:15 +0900

    [32기임종혁]초기세팅완료

---
+++ src/Main/Main.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Main = () => {
+  return (
+    <>
+      <StyledHeader>hello world</StyledHeader>
+    </>
+  );
+};
+
+const StyledHeader = styled.div`
+  color: red;
+`;
+export default Main;
